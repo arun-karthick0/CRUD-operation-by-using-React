5d8fa3d6443c8743b17ee43a113ddad67791f714
Arun <arunkarthick0810@gmail.com>
2023-06-14 12:24:18 +0530

    first commit

---
+++ src/components/Navbar.js
@@ -0,0 +1,14 @@
+import React,{useContext} from "react";
+import { TasksContext } from './../context/TasksContext';
+
+const Navbar = () => {
+  const {state} = useContext(TasksContext)
+  return (
+    <>
+      <h1 className="text-center my-4 text-primary">Project Management</h1>
+      <p className="text-center lead">{`Currently ${state.tasksList.length} task(s) pending`}</p>
+    </>
+  );
+};
+
+export default Navbar;
